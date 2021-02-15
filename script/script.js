43050684ae226580ac337bcdae34e94746865512
cristian-bars <patxi.cristian@gmail.com>
2021-02-15 19:00:49 +0100

    Calculadora sense eval

--- script/script.js
+++ script/script.js
@@ -64,3 +64,75 @@ function calculadora(operador, num1, num2){
 
 let resultat = 0;
 calculadora('multiplicacio',40,20);
+
+
+let operandoa = "";
+let operandob = "";
+let operacion = "";
+
+function add(key) { 
+    if(operandoa!=""){
+        result.textContent = "";
+    }
+    result.textContent = result.textContent + key;
+}
+
+function del(){
+    resetear();
+}
+function suma(){
+    operandoa = result.textContent;
+    operacion = "+";
+    limpiar();
+}
+function resta(){
+    operandoa = result.textContent;
+    operacion = "-";
+    limpiar();
+}
+function multiplicacion(){
+    operandoa = result.textContent;
+    operacion = "*";
+    limpiar();
+}
+function division(){
+    operandoa = result.textContent;
+    operacion = "/";
+    limpiar();
+}
+
+function calc(){
+    operandob = result.textContent;
+    resolver();
+}
+
+function limpiar(){
+    result.textContent = result.textContent;
+}
+
+function resetear(){
+    result.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+  
+function resolver(){
+    var res = 0;
+    switch(operacion){
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    result.textContent = res;
+}
